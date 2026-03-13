6c069707fc00552f8a02280dc52c42813c875a64
Adam Labruyere <adam.labruyere@gmail.com>
2026-03-13 12:56:43 +0100

    add path

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from pathlib import Path
 
-st.set_page_config(page_title="Mon dashboard", layout="wide")
 
-st.title("Dashboard interactif en Python")
+st.set_page_config(page_title="Où vivre en 2040 ?", layout="wide")
+
+st.title("Dashboard interactif")
+
+# emplois et chomage
+chomage_2000_2025_dep = "tables/emplois et chomage/Demandeurs_emploi_taux_chomage_2000_2025.xlsx"
+emplois_par_ststus_secteur = "tables/emplois et chomage/ECRT2024_F1.xlsx"
+emplois_par_region = "tables/emplois et chomage/emplois en 2020 par region.xlsx"
+
+# etudiants
+etudes_sup_dep_reg_2001_2023 = "tables/etudiants/fr-esr-atlas_regional-effectifs-d-etudiants-inscrits_agregeables.xlsx"
+
+# revenu
+disparité_de_revenus = "tables/revenu/base-cc-filosofi-2021-geo2025.xlsx"
+déclaratif_par_commune = "tables/revenu/FILO2021_DEC_COM.xlsx"
+dispo_par_commune = "tables/revenu/FILO2021_DISP_COM.xlsx"
+
+# santé
+données_accès_hopitaux = "tables/Santé/analyse_acces_hopital_2010.xlsx"
+couverture_medicale_2024 = "tables/Santé/santé data.xlsx"
+
 
 # Jeu de données simple
 df = pd.DataFrame({
